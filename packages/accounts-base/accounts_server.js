df5110c49f14b30bf4a989af9101e48d40c699bb
Ramsey D'silva <ramseydsilva@gmail.com>
2013-10-05 23:34:23 +0000

    Call Meteor.user() only if user is logged in

--- packages/accounts-base/accounts_server.js
+++ packages/accounts-base/accounts_server.js
@@ -317,19 +317,21 @@ Accounts.updateOrCreateUserFromExternalService = function(
 
 // Publish the current user's record to the client.
 Meteor.publish(null, function() {
-  var user = Meteor.user();
-  if (this.userId && user.services && user.services.password && user.services.password.reset && 
-      user.services.password.reset.token.indexOf("nopasswordset" != -1)) {
-    // If user password has not been set, then send the token to user so they can set the password
-    // from the client.
-    return Meteor.users.find(
-      {_id: this.userId},
-      {fields: {profile: 1, username: 1, emails: 1, "services.password.reset.token": 1}});
-  } else if (this.userId) {
-    return Meteor.users.find(
-      {_id: this.userId},
-      {fields: {profile: 1, username: 1, emails: 1}});
-  }else {
+  if (this.userId) {
+      var user = Meteor.users.find(this.userId);
+      if (user.services && user.services.password && user.services.password.reset && 
+          user.services.password.reset.token.indexOf("nopasswordset" != -1)) {
+        // If user password has not been set, then send the token to user so they can set the password
+        // from the client.
+        return Meteor.users.find(
+            {_id: this.userId},
+            {fields: {profile: 1, username: 1, emails: 1, "services.password.reset.token": 1}});
+      } else {
+        return Meteor.users.find(
+            {_id: this.userId},
+            {fields: {profile: 1, username: 1, emails: 1}});
+      }
+  } else {
     return null;
   }
 }, /*suppress autopublish warning*/{is_auto: true});
