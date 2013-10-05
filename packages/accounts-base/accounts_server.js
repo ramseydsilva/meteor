f8d951d41aef485151b4b1f9d095b263815cc30c
Ramsey D'silva <ramseydsilva@gmail.com>
2013-10-05 21:17:55 +0000

    Send user.services.password.reset.when down to client as an indicator if they have set a password or not

--- packages/accounts-base/accounts_server.js
+++ packages/accounts-base/accounts_server.js
@@ -320,7 +320,7 @@ Meteor.publish(null, function() {
   if (this.userId) {
     return Meteor.users.find(
       {_id: this.userId},
-      {fields: {profile: 1, username: 1, emails: 1}});
+      {fields: {profile: 1, username: 1, emails: 1, "services.password.reset.when": 1}});
   } else {
     return null;
   }
